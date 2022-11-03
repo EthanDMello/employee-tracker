850168915bef32835c1fc816229b5e977d434e57
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-11-03 16:26:38 +0000

    new schema added with pri/foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -3,23 +3,25 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee(
+CREATE TABLE departments(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  employee_first_name VARCHAR(30) NOT NULL,
-  employee_last_name VARCHAR(30) NOT NULL,
-  employee_role VARCHAR(30) FOREIGN KEY,
-  manager VARCHAR(30) NOT NULL,
-  salary INT
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   role_name VARCHAR(30) NOT NULL,
   salary INT NOT NULL,
-  department_id VARCHAR(30) FOREIGN KEY
+  department_id INT NOT null,
+  FOREIGN KEY (department_id) REFERENCES departments(id)  
 );
 
-CREATE TABLE departments(
+CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  employee_first_name VARCHAR(30) NOT NULL,
+  employee_last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager VARCHAR(30) NOT null,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
 );
